7364c9466184cebb81081fb9789df167e6d3bb98
martin-ngigi <martinwainaina001@gmail.com>
2022-10-02 16:20:02 +0300

    Debugging

--- playground/views.py
+++ playground/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 # a view takes in a request and returns a response
+def calculate():
+    x=1
+    y=2
+    return x
 
 def sey_Hello(request):
+    x=calculate()
     return render(request, 'hello.html', {'name':''})
